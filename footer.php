979e0e549911ebdfdcfe64a2a6d4c2b2da88638d
Enzo Vergara <enzoamb123@gmail.com>
2021-02-22 19:40:22 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
 
+
+  <script src="https://unpkg.com/bluebird@3.7.2/js/browser/bluebird.core.min.js"></script>
+  <!-- <script src="https://unpkg.com/whatwg-fetch@3.4.1/dist/fetch.umd.js"></script> -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/systemjs/6.8.3/system.min.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/assets/homepage-legacy.4f8b63db.js"></script>
 
     <?php wp_footer(); ?>
