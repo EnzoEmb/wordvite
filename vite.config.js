07eafa2091dcf8569eb6ee0daf5678017c9702b5
Enzo Vergara <enzoamb123@gmail.com>
2021-02-22 21:04:18 -0300

    testing build folder

--- vite.config.js
+++ vite.config.js
@@ -16,22 +16,22 @@ export default {
 
   build: {
     outDir: resolve(__dirname, 'assets/'),
-    emptyOutDir: false,
     manifest: true,
-    target: 'es2018',
+    // target: 'es2018',
     // our entry
+    assetsDir: './',
     rollupOptions: {
       input: {
         homepage: resolve(__dirname, 'src/js/homepage.js'),
         page_1: resolve(__dirname, 'src/js/page_1.js'),
     //     asdasd: resolve(__dirname, 'src/css/app.css')
       },
-      output: {
+      // output: {
 
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
-      }
+      //   entryFileNames: `[name].js`,
+      //   chunkFileNames: `[name].js`,
+      //   assetFileNames: `[name].[ext]`
+      // }
     }
   },
 }
